e3829d069fdfe654e57f11ca0455d1e1b7afeff2
Jesse Billard <jessebillard@mac.com>
2018-01-23 22:09:44 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,6 +2,6 @@ var playlist = {
   artistNames : 'songTitle'
 }
 
-function updatePlaylist(playlist, 'artistNames', 'songTitle') {
-  playlist['Phil Ochs'] = 'Here\'s 
+function updatePlaylist(playlist, artistNames, songTitle) {
+  return Object.assign({}, playlist, { [Phil Ochs]: "Here\'s to the State of Mississippi"})
 }
